63176e4992f0197b45c0631f64c9ffa5fe65cde3
ridhasnow <threshsupprt@gmail.com>
2025-12-17 17:51:24 +0100

    Create App component with HomePage integration

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import HomePage from './pages/HomePage'
+import './styles/App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <HomePage />
+    </div>
+  )
+}
+
+export default App
