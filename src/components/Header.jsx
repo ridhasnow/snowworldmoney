a3509ac53dd4c6185bdef27ff44bd839c385b272
ridhasnow <threshsupprt@gmail.com>
2025-12-17 20:15:01 +0100

    Create Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/Header.css'
+
+export default function Header() {
+  return (
+    <div className="header-banner">
+      <div className="header-content">
+        <h1 className="header-title">مرحباً بكم في الموقع الرسمي الأول في تونس للتحويل الرقمي</h1>
+      </div>
+    </div>
+  )
+}
