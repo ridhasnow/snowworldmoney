da7c28e10e213b1031e79b58eaa958408ac87ec0
ridhasnow <threshsupprt@gmail.com>
2025-12-21 21:39:17 +0100

    Use siteConfig for logo URL in Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import '../styles/Headers.css'
+import siteConfig from '../config/siteConfig'
 
 export default function Header() {
-  const logoUrl = import.meta.env.VITE_LOGO_URL // إذا غير مُعرّف، اللوغو لن يظهر
-
   return (
     <div className="header-banner">
       <div className="header-content">
@@ -11,11 +10,11 @@ export default function Header() {
           مرحباً بكم في الموقع الرسمي الأول في تونس للتحويل الرقمي
         </h1>
 
-        {/* مكان ثابت لشعار الموقع في أقصى اليمين (اختياري) */}
-        {logoUrl && (
+        {/* نعرض اللوجو فقط إذا فيه رابط مضبوط */}
+        {siteConfig.logoUrl && (
           <img
             className="header-logo"
-            src={logoUrl}
+            src={siteConfig.logoUrl}
             alt="شعار الموقع"
             loading="lazy"
           />
